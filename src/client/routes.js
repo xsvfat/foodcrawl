325971cfc5d5c786da00ba8abee98ae1cbd6788a
Chris Chiang <cchiang1992@gmail.com>
2016-08-31 22:49:59 -0700

    added default "/main" state with dummy template
    changed dependency from ui-router to ui.router.. was causing errors

--- src/client/routes.js
+++ src/client/routes.js
@@ -1,8 +1,15 @@
-var app = angular.module('foodfood', ['ui-router']);
+var app = angular.module('foodfood', ['ui.router']);
 
-app.config(function($stateProvider, $urlRouterProvider) {
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/main');
+
   $stateProvider
   .state('state1', {
-    template: ''
+    template: '<p>testing state 1</p>'
   })
-})
+  .state('main', {
+    url: '/main',
+    template: '<p>testing main</p>'
+  });
+}])
