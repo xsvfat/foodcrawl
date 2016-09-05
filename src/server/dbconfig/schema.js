8a2a31f0e670f39ca502d5f8a71b5cef17acbe11
Chris Chiang <cchiang1992@gmail.com>
2016-09-05 15:40:24 -0700

    setup basic userSchema

--- src/server/dbconfig/schema.js
+++ src/server/dbconfig/schema.js
@@ -1,2 +1,11 @@
 var mongoose = require('mongoose');
 
+var userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  restaurants: [{
+    name: String,
+    visits: Number
+  }],
+  addresses: [String]
+})
