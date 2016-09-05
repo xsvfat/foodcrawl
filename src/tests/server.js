30109804f50efdd9321acb57c1b93ae58da67927
Chris Chiang <cchiang1992@gmail.com>
2016-09-04 22:34:44 -0700

    added a test

--- src/tests/server.js
+++ src/tests/server.js
@@ -1,9 +1,9 @@
 var chai = require('chai');
 var expect = chai.expect;
 
-describe('Client', function() {
+describe('Server', function() {
 
-  describe('handlers.js', function() {
+  describe('Route handlers', function() {
     var handlers = require('../server/handlers.js');
 
     describe('getRoutes', function() {
@@ -20,14 +20,31 @@ describe('Client', function() {
        * it should provide all the required props to a ajax req
        * it should throw an error upon ajax failure
        */
+       it('should return an object', function() {
+        var result = handlers.getRoutes('San Francisco', 'San Jose');
+        expect(result).to.be.instanceof(Object);
+       });
     });
 
     describe('submit', function() {
+
       it('should be a function', function() {
         expect(handlers.getRoutes).to.be.a('function');
+      });
+
+      it('should throw an error for empty arguments', function() {
+        // fix this test
+        expect(false).to.equal(true);
       })
     });
+
+    describe('getRestaurants', function() {
+      
+      it ('should be a function', function() {
+        expect(handlers.getRestaurants).to.be.a('function');
+      })
+    })
+
   });
 
-  //Add more subsections
 });
