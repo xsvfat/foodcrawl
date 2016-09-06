f5577d6fd55f15d23daed97fc03a464a32cac1c5
Chris Chiang <cchiang1992@gmail.com>
2016-09-05 18:04:11 -0700

    removed example state.. was only used for testing purposes earlier

--- src/client/routes.js
+++ src/client/routes.js
@@ -5,9 +5,6 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
   $urlRouterProvider.otherwise('/main');
 
   $stateProvider
-  .state('state1', {
-    template: '<p>testing state 1</p>'
-  })
   .state('main', {
     url: '/main',
     templateUrl: './views/main.html',
