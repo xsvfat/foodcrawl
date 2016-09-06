f70028e9e3292de9ec03b1676f59c17b16153406
Chris Chiang <cchiang1992@gmail.com>
2016-09-06 12:43:32 -0700

    Auth service manipulates $localStorage
    Don't need to use $localStorage in controllers;
    script tag added to index.html;

--- src/client/services/authStorage.js
+++ src/client/services/authStorage.js
@@ -1,14 +1,15 @@
 app.factory('Auth', ['$localStorage', function($localStorage) {
-  var session = {
-    username: null,
-    password: null
-  }
+
   return {
     store: (username, password) => {
-
+      $localStorage.username = username;
     },
     check: () => {
-
+      return $localStorage.username;
     },
+    delete: () => {
+      delete $localStorage.username;
+    }
   }
+  
 }]);
