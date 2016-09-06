ba8735930ac161d26ca834ab0c112db4f672d917
Chris Chiang <cchiang1992@gmail.com>
2016-09-06 12:18:03 -0700

    added authStorage.js for a new auth service

--- src/client/routes.js
+++ src/client/routes.js
@@ -1,4 +1,4 @@
-var app = angular.module('foodfood', ['ui.router', 'ngSanitize']);
+var app = angular.module('foodfood', ['ui.router', 'ngSanitize', 'ngStorage']);
 
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
