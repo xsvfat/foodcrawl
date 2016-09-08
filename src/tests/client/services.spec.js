826f920d78ae2705760772665f5bc3abfced56e7
Alex Pattison <alexlpattison@gmail.com>
2016-09-08 13:30:15 -0700

    Implement tests to check if functions exist

--- src/tests/client/services.spec.js
+++ src/tests/client/services.spec.js
@@ -3,14 +3,39 @@ describe('Services', function() {
   beforeEach(module('foodfood'));
 
   describe('RestaurantAndRoute factory')
-  var $httpBackend, Links;
+  var $httpBackend, RestaurantAndRoute;
 
     beforeEach(inject(function (_$httpBackend_, _RestaurantAndRoute_) {
       $httpBackend = _$httpBackend_;
       RestaurantAndRoute = _RestaurantAndRoute_;
     }));
 
-    it('should exist for real', function () {
+    it('should exist', function () {
       expect(RestaurantAndRoute).to.exist;
     });
+
+    it('should have a method `fetchRestaurants`', function () {
+      expect(RestaurantAndRoute.fetchRestaurants).to.be.a('function');
+    });
+
+    it('should have a method `getRestaurants`', function () {
+      expect(RestaurantAndRoute.getRestaurants).to.be.a('function');
+    });
+
+    it('should have a method `addMarkers`', function () {
+      expect(RestaurantAndRoute.addMarkers).to.be.a('function');
+    });
+
+    it('should have a method `removeMarkers`', function () {
+      expect(RestaurantAndRoute.removeMarkers).to.be.a('function');
+    });
+
+    it('should have a method `calculateAndDisplayRoute`', function () {
+      expect(RestaurantAndRoute.calculateAndDisplayRoute).to.be.a('function');
+    });
+
+    it('should have a method `openInfoWindow`', function () {
+      expect(RestaurantAndRoute.openInfoWindow).to.be.a('function');
+    });
+
 })
