9434a6c132d1eed35875763a0df253c1288f3e4b
Alex Pattison <alexlpattison@gmail.com>
2016-09-07 17:35:14 -0700

    Implement example to test in browser
    I must be missing some dependency. It says app is not defined in controller.js.

---
+++ src/tests/client/example.js
@@ -0,0 +1,22 @@
+describe('Examples', function() {
+
+  //'describe' statements can be nested to create sub-sections
+  describe('someFunction()', function() {
+
+    //'it' statements describe what the test is checking for
+    //Descriptions should describe the intended behavior
+    it('should be a string', function() {
+
+      //Check type
+      var foo = 'example';
+      expect(foo).to.be.a('string');
+    });
+
+    it('should equal some value', function() {
+
+      //Check equality
+      var foo = 'bar';
+      expect(foo).to.equal('bar');
+    });
+  });
+});
