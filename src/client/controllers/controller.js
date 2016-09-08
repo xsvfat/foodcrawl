7791fff4f3e812d3f3383d57ce63ba2364c0cefb
ZolayvarE <ZolayvarECU@gmail.com>
2016-09-08 14:09:23 -0700

    Set walking as default travel mode

--- src/client/controllers/controller.js
+++ src/client/controllers/controller.js
@@ -12,6 +12,7 @@ app.controller('inputsController', ['$scope', '$http', '$state', '$sce', 'Restau
     $scope.end; // end location input
     $scope.map; //store map
     $scope.directions = ''; // directions from start to end
+    $scope.mode = 'walking';
 
     $scope.logout = () => {
       Auth.delete();
