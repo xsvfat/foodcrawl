dccadfe60bda9789e7c1aade84e4d249885b7927
Justin Griffin <jbg46209@gmail.com>
2016-09-10 15:26:33 -0700

    Fix bug info windows not displaying correctly after new query

--- src/client/controllers/controller.js
+++ src/client/controllers/controller.js
@@ -180,6 +180,8 @@ app.controller('inputsController', ['$scope', '$http', '$state', '$sce', 'Restau
 
     // POST users' start and end locations to server
     $scope.submit = function(form) {
+      //clear old data
+      RestaurantAndRoute.clearStoredRestaurants();
 
       // to refresh states from main.map, need to redirect to main first
       $state.go('main');
