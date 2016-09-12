99c8c56a4ba36d6ef60ec858748c26e22d556bd9
Alex Pattison <alexlpattison@gmail.com>
2016-09-12 14:53:25 -0700

    Add karma start to package.json and delete non-working tests/examples

--- src/tests/client/controller.spec.js
+++
@@ -1,3 +0,0 @@
-describe('inputsController', function() {
-  
-});
--- src/tests/client/services.spec.js
+++ src/tests/client/services.spec.js
@@ -1,12 +1,12 @@
 describe('Services', function() {
   // Run these tests with karma start from the command line
-  
+
   // Import the corresponding module
   beforeEach(module('foodfood'));
 
   describe('RestaurantAndRoute factory', function() {
     var $httpBackend, RestaurantAndRoute;
-
+    
     // inject the necessary dependencies
     beforeEach(inject(function (_$httpBackend_, _RestaurantAndRoute_) {
       $httpBackend = _$httpBackend_;
@@ -16,50 +16,6 @@ describe('Services', function() {
     // these tests just test for the existence of our methods
     it('should exist', function() {
       expect(RestaurantAndRoute).to.exist;
-    });
-
-    describe('fetchRestaurants', function() {
-      it('should have a method `fetchRestaurants`', function() {
-        expect(RestaurantAndRoute.fetchRestaurants).to.be.a('function');
-      });
-
-      it('should return a list of restaurants', function() {
-        var mockResponse = [
-        { name: 'Delicious Restaurant',
-          rating: 5 },
-        { title: 'Bad Restaurant',
-          rating: 1 }
-        ];
-
-        $httpBackend.expect('POST', '/maps/submit').respond(mockResponse);
-
-        // I'm running into problems right now due to login
-        // It's posting a GET request to .views/login/html
-        RestaurantAndRoute.fetchRestaurants().then(function (restaurants) {
-          expect(restaurants).to.deep.equal(mockResponse);
-        });
-
-        $httpBackend.flush();
-      });
-    });
-
-    describe('getRestaurants', function() {
-      it('should have a method `getRestaurants`', function() {
-        expect(RestaurantAndRoute.getRestaurants).to.be.a('function');
-      });
-      // These methods are all hard to test because they rely on side effects
-      it('should return a list of restaurants', function() {
-        var restaurants = [
-          { name: 'Delicious Restaurant',
-            rating: 5 },
-          { title: 'Bad Restaurant',
-            rating: 1 }
-        ];
-        /*
-          This will return an empty array at the moment because restaurants is an empty array in services.js. I need to figure out a way to call fetchRestaurants with some mock data in order to populate the restaurants array.
-        */
-        expect(RestaurantAndRoute.getRestaurants()).to.equal(restaurants);
-      });
     });
 
     it('should have a method `addMarkers`', function() {
