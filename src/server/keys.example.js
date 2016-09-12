50fa181b3b3a4275e8b9bf91adfcc9e7b14d93b2
ZolayvarE <ZolayvarECU@gmail.com>
2016-09-12 14:50:52 -0700

    Add example keys.js file

---
+++ src/server/keys.example.js
@@ -0,0 +1,8 @@
+// Populate this object with your own API Keys.
+module.exports = {
+  googleMaps: 'YOUR_GOOGLE_MAPS_API_KEY',
+  yelp: 'YOUR_YELP_API_KEY',
+  yelpSecret: 'YOUR_YELP_CONSUMER_SECRET',
+  yelpToken: 'YOUR_YELP_TOKEN',
+  yelpTokenSecret: 'YOUR_YELP_TOKEN_SECRET',
+};
