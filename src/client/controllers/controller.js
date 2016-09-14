0e0792e3d0e8db81dc7c9952b585fb886b2a3c16
Tim Z <tjkz87@gmail.com>
2016-09-14 14:18:40 -0700

    fixed navbar. sidebar close issues

--- src/client/controllers/controller.js
+++ src/client/controllers/controller.js
@@ -1,11 +1,5 @@
 // controller for start & end inputs
 app.controller('inputsController', ['$scope', '$http', '$state', 'RestaurantAndRoute', 'Auth', '$localStorage', 'Addresses', function($scope, $http, $state, RestaurantAndRoute, Auth, $localStorage, Addresses) {
-  
-  Materialize.updateTextFields(); // solves input field placeholder overlapping issue
-  $('select').material_select(); // solves select issues
-  $(".button-collapse").sideNav({
-    closeOnClick: true
-  });
 
   $scope.start = ''; // start location input
   $scope.end = ''; // end location input
